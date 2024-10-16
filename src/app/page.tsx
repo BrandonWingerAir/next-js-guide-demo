657391efae5dee7e993141d0469133018b9b0cfe
Brandon Winger-Air <brandonwinger-air@outlook.com>
2024-10-16 12:08:32 -0400

    public folder images and home background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import Image from 'next/image';
+import homeImage from '/public/images/home.jpg';
+
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      Home Page
+
+      <div className="absolute -z-10 inset-0">
+        <Image
+          src={homeImage}
+          alt="car factory"
+          fill
+          style={{ objectFit: 'cover' }}
+        />
+      </div>
+    </div>
   );
 }
